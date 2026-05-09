5a04289528855dc926f0adca0a0cd245951572ef
Lautaro Rodriguez <lautarohrodriguez1@gmail.com>
2026-05-09 10:45:23 +0200

    Faqs

--- src/app/features/info/faqs/faqs.component.ts
+++ src/app/features/info/faqs/faqs.component.ts
@@ -1,6 +1,12 @@
-import { Component, ChangeDetectionStrategy } from '@angular/core';
+import { Component, ChangeDetectionStrategy, signal } from '@angular/core';
 import { TranslocoModule } from '@jsverse/transloco';
 import { BreadcrumbComponent, BreadcrumbItem } from '../../../shared/components/breadcrumb/breadcrumb.component';
+
+interface FaqItem {
+  q: string;
+  a: string;
+  open: ReturnType<typeof signal<boolean>>;
+}
 
 @Component({
   selector: 'app-faqs',
@@ -8,7 +14,18 @@ import { BreadcrumbComponent, BreadcrumbItem } from '../../../shared/components/
   standalone: true,
   imports: [TranslocoModule, BreadcrumbComponent],
   templateUrl: './faqs.component.html',
+  styleUrl: './faqs.component.css',
 })
 export class FaqsComponent {
   readonly breadcrumbs: BreadcrumbItem[] = [{ labelKey: 'footer.faqs' }];
+
+  readonly items: FaqItem[] = Array.from({ length: 15 }, (_, i) => ({
+    q: `faqs.q${i + 1}`,
+    a: `faqs.a${i + 1}`,
+    open: signal(false),
+  }));
+
+  toggle(item: FaqItem): void {
+    item.open.update(v => !v);
+  }
 }
