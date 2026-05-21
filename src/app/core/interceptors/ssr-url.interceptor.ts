d2a51d480d64af95fad1dc8e388923c1d0828f04
Lautaro Rodriguez <lautarohrodriguez1@gmail.com>
2026-05-21 22:23:15 +0200

    Prueba fix SSR 3

--- src/app/core/interceptors/ssr-url.interceptor.ts
+++ src/app/core/interceptors/ssr-url.interceptor.ts
@@ -3,8 +3,14 @@ import { inject, PLATFORM_ID } from '@angular/core';
 import { isPlatformServer } from '@angular/common';
 
 export const ssrUrlInterceptor: HttpInterceptorFn = (req, next) => {
-  if (isPlatformServer(inject(PLATFORM_ID)) && req.url.startsWith('https://pondoxa.com')) {
-    return next(req.clone({ url: req.url.replace('https://pondoxa.com', 'http://nginx') }));
+  if (isPlatformServer(inject(PLATFORM_ID))) {
+    let url = req.url;
+    if (url.startsWith('https://pondoxa.com')) {
+      url = url.replace('https://pondoxa.com', 'http://nginx');
+    } else if (url.startsWith('/')) {
+      url = `http://nginx${url}`;
+    }
+    return next(req.clone({ url }));
   }
   return next(req);
 };
