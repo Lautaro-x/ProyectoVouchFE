43b1df99c06255228f7c41ee1654789ddd665d4d
Lautaro Rodriguez <lautarohrodriguez1@gmail.com>
2026-05-21 22:31:20 +0200

    Prueba fix SSR 4 Esto es personal

--- src/app/core/interceptors/ssr-url.interceptor.ts
+++ src/app/core/interceptors/ssr-url.interceptor.ts
@@ -2,13 +2,17 @@ import { HttpInterceptorFn } from '@angular/common/http';
 import { inject, PLATFORM_ID } from '@angular/core';
 import { isPlatformServer } from '@angular/common';
 
+const NGINX = 'http://nginx';
+const ORIGINS = ['https://pondoxa.com', 'http://localhost'];
+
 export const ssrUrlInterceptor: HttpInterceptorFn = (req, next) => {
   if (isPlatformServer(inject(PLATFORM_ID))) {
     let url = req.url;
-    if (url.startsWith('https://pondoxa.com')) {
-      url = url.replace('https://pondoxa.com', 'http://nginx');
+    const origin = ORIGINS.find(o => url.startsWith(o));
+    if (origin) {
+      url = `${NGINX}${url.slice(origin.length)}`;
     } else if (url.startsWith('/')) {
-      url = `http://nginx${url}`;
+      url = `${NGINX}${url}`;
     }
     return next(req.clone({ url }));
   }
