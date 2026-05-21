f95c0aff1199bd23baf438db4ece0dcc258331d5
Lautaro Rodriguez <lautarohrodriguez1@gmail.com>
2026-05-21 21:18:29 +0200

    anuncios

--- src/app/shared/components/ad-banner/ad-banner.component.ts
+++ src/app/shared/components/ad-banner/ad-banner.component.ts
@@ -1,4 +1,4 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { afterNextRender, ChangeDetectionStrategy, Component, input } from '@angular/core';
 import { TranslocoModule } from '@jsverse/transloco';
 
 @Component({
@@ -9,4 +9,17 @@ import { TranslocoModule } from '@jsverse/transloco';
   templateUrl: './ad-banner.component.html',
   styleUrl: './ad-banner.component.css',
 })
-export class AdBannerComponent {}
+export class AdBannerComponent {
+  adSlot = input.required<string>();
+
+  readonly adClient = 'ca-pub-9130792052986864';
+
+  constructor() {
+    afterNextRender(() => {
+      try {
+        (window as any)['adsbygoogle'] = (window as any)['adsbygoogle'] || [];
+        (window as any)['adsbygoogle'].push({});
+      } catch {}
+    });
+  }
+}
